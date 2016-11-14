70ea9a897eeb9ed35d520e251c043bf9345d91e4
Hiroshi Hatake <hatake@clear-code.com>
2016-11-14 13:37:48 +0900

    out_rewrite: Mark tag manipulation config_params as deprecated

--- lib/fluent/plugin/out_rewrite.rb
+++ lib/fluent/plugin/out_rewrite.rb
@@ -6,8 +6,10 @@ module Fluent::Plugin
 
     helpers :event_emitter
 
-    config_param :remove_prefix,   :string, :default => nil
-    config_param :add_prefix,      :string, :default => nil
+    config_param :remove_prefix,   :string, :default => nil,
+                 deprecated: "use @label instead for event routing"
+    config_param :add_prefix,      :string, :default => nil,
+                 deprecated: "use @label instead for event routing"
     config_param :enable_warnings, :bool,   :default => false
 
     attr_reader  :rewrite_rule
